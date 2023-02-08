5074a5dd7f0e7a192442874663b5263bb9166abe
Yaroslav-Zelenyi <zelenyiyaroslav@gmail.com>
2023-02-08 21:48:10 +0200

    Create task

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,9 @@
-const categoriesElement = document.querySelector("#categories")
-console.log('Number of categories: ',categoriesElement.children.length );
+const categoriesElement = document.querySelector("#categories");
+console.log("Number of categories: ", categoriesElement.children.length);
+
 for (const item of categoriesElement.children) {
-    console.log(`Category: ${item.firstElementChild.textContent}Elements: ${item.lastElementChild.children.length}`);
+  console.log(
+    `Category: ${item.firstElementChild.textContent} Elements: ${item.lastElementChild.children.length}`
+  );
 }
+
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listEls = [];
+const itemEl = document.querySelector("#ingredients");
+
+for (const ingredient of ingredients) {
+  const liEl = document.createElement("li");
+  liEl.textContent = ingredient;
+  liEl.classList.add("item");
+  listEls.push(liEl);
+}
+
+itemEl.append(...listEls);
+
